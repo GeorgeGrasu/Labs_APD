1628490e46836c1c0ba2c7be44a3d08bf2e60bf3
GeorgeGrasu <grasugeorgesebastian@gmail.com>
2025-10-27 23:32:11 +0200

    first commit

---
+++ laboratoare/lab03/oets.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <math.h>
+#include "pthread_barrier_mac.h"
+
+int N;
+int P;
+int *v;
+int *vQSort;
+pthread_barrier_t barrier;
+
+void compare_vectors(int *a, int *b) {
+	int i;
+
+	for (i = 0; i < N; i++) {
+		if (a[i] != b[i]) {
+			printf("Sortare incorecta\n");
+			return;
+		}
+	}
+
+	printf("Sortare corecta\n");
+}
+
+void display_vector(int *v) {
+	int i;
+	int display_width = 2 + log10(N);
+
+	for (i = 0; i < N; i++) {
+		printf("%*i", display_width, v[i]);
+	}
+
+	printf("\n");
+}
+
+int cmp(const void *a, const void *b) {
+	int A = *(int*)a;
+	int B = *(int*)b;
+	return A - B;
+}
+
+void get_args(int argc, char **argv)
+{
+	if(argc < 3) {
+		printf("Numar insuficient de parametri: ./oets N P\n");
+		exit(1);
+	}
+
+	N = atoi(argv[1]);
+	P = atoi(argv[2]);
+}
+
+void init()
+{
+	int i;
+	v = malloc(sizeof(int) * N);
+	vQSort = malloc(sizeof(int) * N);
+
+	if (v == NULL || vQSort == NULL) {
+		printf("Eroare la malloc!");
+		exit(1);
+	}
+
+	srand(42);
+
+	for (i = 0; i < N; i++)
+		v[i] = rand() % N;
+}
+
+void print()
+{
+	printf("v:\n");
+	display_vector(v);
+	printf("vQSort:\n");
+	display_vector(vQSort);
+	compare_vectors(v, vQSort);
+}
+
+void *thread_function(void *arg)
+{
+	int thread_id = *(int *)arg;
+
+	// TODO: implementati aici OETS paralel
+	for(int phase = 0; phase < N; phase++) {
+		if(phase % 2 == 0) {
+			for(int i = 2 * thread_id; i < N - 1; i += 2 * P) {
+				if(v[i] > v[i + 1]) {
+					int aux = v[i];
+					v[i] = v[i + 1];
+					v[i + 1] = aux;
+				}
+			}
+		} else {
+			for(int i = 2 * thread_id + 1; i < N - 1; i += 2 * P) {
+				if(v[i] > v[i + 1]) {
+					int aux = v[i];
+					v[i] = v[i + 1];
+					v[i + 1] = aux;
+				}
+			}
+		}
+		pthread_barrier_wait(&barrier);
+	}
+
+	pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+	get_args(argc, argv);
+	init();
+
+	int i;
+	pthread_t tid[P];
+	int thread_id[P];
+
+	// se sorteaza vectorul etalon
+	for (i = 0; i < N; i++)
+		vQSort[i] = v[i];
+	qsort(vQSort, N, sizeof(int), cmp);
+
+	// se creeaza thread-urile
+	pthread_barrier_init(&barrier, NULL, P);
+	for (i = 0; i < P; i++) {
+		thread_id[i] = i;
+		pthread_create(&tid[i], NULL, thread_function, &thread_id[i]);
+	}
+
+	// se asteapta thread-urile
+	for (i = 0; i < P; i++) {
+		pthread_join(tid[i], NULL);
+	}
+
+	pthread_barrier_destroy(&barrier);
+
+	// bubble sort clasic - trebuie transformat in OETS si paralelizat
+	// se afiseaza vectorul etalon
+	// se afiseaza vectorul curent
+	// se compara cele doua
+	print();
+
+	free(v);
+	free(vQSort);
+
+	return 0;
+}
